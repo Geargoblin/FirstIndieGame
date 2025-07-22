cfa4d1460b1452c2153f204f9e54c13ab364ef47
Jacob Hall <Geargoblin1@gmail.com>
2025-07-21 20:22:45 -0400

    Started Creating Framework
    Added a Card script for collecting information and created a folder for scripts and UI

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public int Cost;
+    public string Type;
+    public int Delay;
+    public int Health;
+    public int Attack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
