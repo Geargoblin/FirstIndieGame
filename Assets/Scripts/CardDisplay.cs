af9acc51cd771d43aed060ac657e7c9b699f4deb
Jacob Hall <Geargoblin1@gmail.com>
2025-08-07 16:56:45 -0400

    Created Card Display Script

---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using GearGoblinProductions;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card cardData;
+
+    public Image cardImage;
+    public TMP_Text nameText;
+    public TMP_Text healthText;
+    public TMP_Text damageText;
+    public Image[] typeImages;
+    private Color[] cardColors = {
+        Color.red,
+        Color.blue
+    };
+    private Color[] typeColors = {
+        new Color(0.8f,0f,0f),
+        new Color(0.8f,0f,0.8f),
+    };
+    void Start()
+    {
+        UpdateCardDisplay();
+    }
+
+    public void UpdateCardDisplay()
+    {
+        //Update main cardImage color based on Primary Card Type and Data
+        cardImage.color = cardColors[(int)cardData.cardType[0]];
+
+        nameText.text = cardData.cardName;
+        healthText.text = cardData.health.ToString();
+        damageText.text = $"{cardData.damageMin} - {cardData.damageMax}";
+
+        //Update type images
+        for (int i = 0; i < typeImages.Length; i++) {
+            if (i < cardData.cardType.Count)
+            {
+                typeImages[i].gameObject.SetActive(true);
+                typeImages[i].color = typeColors[(int)cardData.cardType[i]];
+            }
+            else
+            {
+               typeImages[i].gameObject.SetActive(false); 
+            }
+        }
+
+    }
+}
